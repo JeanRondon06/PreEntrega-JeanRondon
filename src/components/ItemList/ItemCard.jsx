43322db15b920a1621618755f34a683150c495cb
jean Rondon <jean.rf06@gmail.com>
2023-05-19 23:30:17 -0500

    cards

--- src/components/ItemList/ItemCard.jsx
+++ src/components/ItemList/ItemCard.jsx
@@ -1,1 +1,34 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
+const ItemCard = ({ item }) => {
+  return (
+    <Card sx={{ width: 345, height: 350 }} key={item.id}>
+      <CardMedia sx={{ height: 140 }} image={item.img} title="green iguana" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" size="small">
+          Ver Detalle
+        </Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ItemCard;
